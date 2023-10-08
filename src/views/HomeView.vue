11b08f476ed94b470f7c596ab1f9988bf54ce79a
M S <mo.sa963@hotmail.com>
2023-10-08 19:02:40 +0300

    Add AppHeader to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue';
+
 </script>
 
 <template>
   <main>
-    <p>home</p>
+    <AppHeader title="Hi, I'm mohamad" info="I am Mohamad, a passionate programmer, who thrives on a full range of programming challenges from
+                    crafting seamless front-end experiences to designing powerful back-end solutions.
+                    For me, programming is not just a profession; it's a boundless journey of creativity and
+                    problem-solving."/>
+
   </main>
 </template>
