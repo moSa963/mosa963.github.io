6f903ad14bf6515993828c1924f44e696999ca6d
M S <mo.sa963@hotmail.com>
2023-10-10 22:38:39 +0300

    Add ProjectsList component

---
+++ src/components/ProjectsList/ProjectsList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    import type { ProjectGroupType } from "../../data/projects";
+import ProjectsGroup from "./ProjectsGroup.vue";
+
+    defineProps<{
+        list: Array<ProjectGroupType>,
+    }>();
+</script>
+
+
+<template>
+    <div class="list">
+        <ProjectsGroup v-for="group in list" :group="group" />
+    </div>
+</template>
+
+
+<style>
+.list{
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
