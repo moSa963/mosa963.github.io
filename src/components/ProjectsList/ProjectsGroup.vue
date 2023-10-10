42041e206f3dc7429c558a52ee7f0287eed2c580
M S <mo.sa963@hotmail.com>
2023-10-10 22:38:13 +0300

    Add ProjectGroup component

---
+++ src/components/ProjectsList/ProjectsGroup.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+    import type { ProjectGroupType } from "../../data/projects";
+import ProjectItem from "./ProjectItem/ProjectItem.vue";
+
+    defineProps<{
+        group: ProjectGroupType,
+    }>();
+</script>
+
+<template>
+    <main>
+        <h1>{{ group.name }}</h1>
+        <p>{{ group.description }}</p>
+        <div class="list">
+            <ProjectItem v-for="project in group.projects" :project="project" />
+        </div>
+    </main>
+</template>
+
+
+<style scoped>
+main{
+    width: 100%;
+}
+h1{
+    font-size: 3rem;
+}
+p{
+    padding-left: 100px;
+    padding-right: 100px;
+    font-size: 1.2rem;
+}
+.list{
+    width: 100%;
+    flex-wrap: wrap;
+    display: flex;
+}
+</style>
