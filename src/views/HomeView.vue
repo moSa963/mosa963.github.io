b62360a106d5a3555c45e77c6c5f771c535fa0f5
M S <mo.sa963@hotmail.com>
2023-10-14 22:17:02 +0300

    Add Background to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
 import AppHeader from '@/components/AppHeader.vue';
+import Background from '@/components/Background.vue';
+
 
 </script>
 
 <template>
-  <main>
-    <AppHeader title="Hi, I'm mohamad" info="I am Mohamad, a passionate programmer, who thrives on a full range of programming challenges from
+    <Background />
+    <main>
+        <AppHeader title="Hi, I'm mohamad" info="I am Mohamad, a passionate programmer, who thrives on a full range of programming challenges from
                     crafting seamless front-end experiences to designing powerful back-end solutions.
                     For me, programming is not just a profession; it's a boundless journey of creativity and
-                    problem-solving."/>
+                    problem-solving." />
 
-  </main>
+    </main>
 </template>
+
+
+<style scoped>
+main {
+    padding-left: 10px;
+    padding-right: 10px;
+}
+
+@media (min-width: 1080px) {
+    main {
+        padding-left: 100px;
+        padding-right: 100px;
+    }
+}
+</style>
+
