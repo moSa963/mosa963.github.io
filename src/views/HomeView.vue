026155eaadf966af389e4b351fb32da209295c38
M S <mo.sa963@hotmail.com>
2023-10-24 21:01:20 +0300

    Add Tools to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,56 +3,44 @@ import AppHeader from '@/components/AppHeader.vue';
 import Background from '@/components/Background.vue';
 import ProjectsList from '@/components/ProjectsList/ProjectsList.vue';
 import { projects } from '@/data/projects';
-import ThemeButton from '@/components/ThemeButton.vue';
+import Tools from '@/components/Tools/Tools.vue';
+import { ref } from 'vue';
+
+const filter = ref<string[]>([]);
+
+const setFilter = (data: string[]) => {
+    filter.value = data;
+}
 
 </script>
 
 <template>
-    <div class="tools">
-        <ThemeButton />
-    </div>
+    <Tools @filter-change="setFilter"/>
     <Background />
     <div class="info">
         <AppHeader title="Hi, I'm mohamad" info="I am Mohamad, a passionate programmer, who thrives on a full range of programming challenges from
         crafting seamless front-end experiences to designing powerful back-end solutions.
         For me, programming is not just a profession; it's a boundless journey of creativity and
         problem-solving." />
-        <ProjectsList :list="projects" />
+        <ProjectsList :list="projects" :filter="filter"/>
     </div>
 </template>
 
 
 <style scoped>
-.tools {
-    padding: 8px;
-    position: fixed;
-    pointer-events: auto;
-}
-
 .info {
     position: absolute;
     margin-left: 10px;
     margin-right: 10px;
+    margin-top: 60px;
     pointer-events: auto;
-}
-
-.icon-button {
-    width: 40px;
-    height: 40px;
-    padding: 5px;
-    cursor: pointer;
-    border-radius: 999px;
-    border: 1px solid;
-}
-
-.icon-button:hover {
-    color: var(--color-heading);
 }
 
 @media (min-width: 1080px) {
     .info {
         margin-left: 100px;
         margin-right: 100px;
+        margin-top: 0px;
     }
 }
 </style>
