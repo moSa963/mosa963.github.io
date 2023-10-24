b9ca8979ab13e4ec4048b81ffd63ad30f88dc389
M S <mo.sa963@hotmail.com>
2023-10-24 20:55:22 +0300

    Add filter prop to ProjectList

--- src/components/ProjectsList/ProjectsList.vue
+++ src/components/ProjectsList/ProjectsList.vue
@@ -1,23 +1,40 @@
 <script setup lang="ts">
-    import type { ProjectGroupType } from "../../data/projects";
+import type { ProjectGroupType } from "../../data/projects";
 import ProjectsGroup from "./ProjectsGroup.vue";
 
-    defineProps<{
-        list: Array<ProjectGroupType>,
-    }>();
+const props = defineProps<{
+    list: Array<ProjectGroupType>,
+    filter?: string[],
+}>();
+
+const checkFilter = (group: ProjectGroupType) : boolean => {  
+    if (!props.filter || props.filter.length === 0) {
+        return true;
+    }
+
+    for(const p of group.projects) {
+        for(const f of props.filter){
+            if (p.technologies.find(e => e == f)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
 </script>
 
 
 <template>
     <div class="list">
-        <ProjectsGroup v-for="group in list" :group="group" />
+        <ProjectsGroup v-for="group in list.filter(checkFilter)" :group="group" />
     </div>
 </template>
 
 
 <style>
-.list{
-  display: flex;
-  flex-wrap: wrap;
+.list {
+    display: flex;
+    flex-wrap: wrap;
 }
 </style>
